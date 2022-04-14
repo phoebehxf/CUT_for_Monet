804d6a50d1983a2f854c923fd48dcaa330bab511
phoebehxf <h_xf@outlook.com>
2022-04-15 03:00:01 +0800

    added readme

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from util.visualizer import Visualizer
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
     opt.save_by_iter = True
-    opt.continue_train = True
+    # opt.continue_train = True
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
 
